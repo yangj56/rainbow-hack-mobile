072192d72c9439c7e74c51ba5c4af514c8ba6725
zhaoyan_xie <zhaoyan.xie@ufinity.com>
2020-01-16 15:58:24 +0800

    Change request screens to tabview

--- lib/screens/requests_screen.dart
+++ lib/screens/requests_screen.dart
@@ -3,59 +3,32 @@ import 'package:rainbow_hack/widgets/menu_drawer.dart';
 import 'package:rainbow_hack/screens/requests_assigned_screen.dart';
 import 'package:rainbow_hack/screens/requests_other_screen.dart';
 
-class RequestsScreen extends StatefulWidget {
+class RequestsScreen extends StatelessWidget {
   static const routeName = '/requests';
-
-  RequestsScreen();
-
-  @override
-  _RequestsScreenState createState() => _RequestsScreenState();
-}
-
-class _RequestsScreenState extends State<RequestsScreen> {
-  List<Map<String, Object>> _pages;
-  int _selectedPageIndex = 0;
-
-  @override
-  void initState() {
-    _pages = [
-      {'page': RequestsAssigned(), 'title': 'My Assigned Requests'},
-      {'page': RequestsOther(), 'title': 'Other Requests'},
-    ];
-    super.initState();
-  }
-
-  void _selectPage(int index) {
-    print(index);
-    setState(() {
-      _selectedPageIndex = index;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(_pages[_selectedPageIndex]['title']),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.star),
-            onPressed: () {
-              _selectPage(0);
-            },
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Groups'),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                text: 'My Assigned',
+              ),
+              Tab(
+                text: 'Others',
+              )
+            ],
           ),
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () {
-              _selectPage(1);
-            },
-          ),
-        ],
+        ),
+        drawer: MainDrawer(),
+        body: TabBarView(
+          children: <Widget>[RequestsAssigned(), RequestsOther()],
+        ),
       ),
-      drawer: Drawer(
-        child: MainDrawer(),
-      ),
-      body: _pages[_selectedPageIndex]['page'],
     );
   }
 }
